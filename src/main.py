fbda31ec251532f47c8d05c704f9b19b25b37913
dgogo <d.gogodze@hotmail,com>
2024-07-11 15:48:57 +0400

    made the template available via api

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
-﻿from fastapi import FastAPI
+﻿from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from jinja2 import Environment, FileSystemLoader
+import markdown as md
+import uvicorn
+
+
+class RequestModel(BaseModel):
+    markdown: str
 
 
 app = FastAPI()
+jinja_env = Environment(loader=FileSystemLoader("Templates"))
+jinja_template = jinja_env.get_template("blog")
+@app.post("/convert/")
+async def convert(markdown: RequestModel):
+    html = md.markdown(markdown.markdown)
+    return jinja_template.render(blog=html)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
